e0e133d581ab7f14e05665d34417defef6a9174a
Elizabeth Marston <elmarsto@microsoft.com>
2020-12-10 00:09:36 -0800

    known good :)

--- magnetite.js
+++ magnetite.js
@@ -1,22 +1,17 @@
 import { Plugin } from 'obsidian';
 import wasm from "./magnetite-guest/Cargo.toml";
 
-const guest = {};
-
-(async () => {
-	Object.assign(guest,await wasm());
-})();
-
-const nonOverridablePropertyNames = [ '__proto__', 'prototype' ];
-// mostly, just avoiding undefined behaviour; I can't reason about what happens
-// to object inheritance (e.g. someone calling super(), or, god forbid, new <...>())
-// the golden rule in networked applications is if you can't reason about it,
-// forbid it, because whatever it is, it's probably a security risk
-
-export default (...args) => {
-	const plugin = new Plugin(...args);
-	const proxy = new Proxy(plugin, {
-		get: (target, propertyName) => nonOverridablePropertyNames.some(propertyName) ? target[propertyName] : guest[propertyName] || target[propertyName],
-	});
-	return proxy;
+export default class Magnetite extends Plugin {
+	constructor(...args) {
+		super(...args);
+		this.guest = wasm();
+	}
+	async onload(...args) {
+		const { onload } = await this.guest;
+		onload(...args);
+	}
+	async onunload(...args) {
+		const { onunload } = await this.guest;
+		onunload(...args);
+	}
 };
